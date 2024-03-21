4b3c0ff6ba25d31b4383272260462a256f770f18
XanderWorld <xander.mymails@gmail.com>
2024-03-21 18:24:08 +0100

    2nd part of About.jsx, cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,28 @@ import {motion} from 'framer-motion';
 import {styles} from "../style.js";
 import {services} from "../constants";
 import {fadeIn, textVariant} from "../utils/motion.js";
+import {Tilt} from "react-tilt";
 
+import {SectionWrapper} from './hoc'
 
 const ServiceCard = ({index, title, icon}) => {
     return (
-        <p>{title}</p>
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div
+                variants={fadeIn("right", "spring", "0.5 * index", "0.75")}
+                className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+            >
+                <div
+                    className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col items-center justify-evenly">
+
+                    <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+                    <h3 className="font-bold text-white text-center text-[20px]">{title}</h3>
+
+                </div>
+
+
+            </motion.div>
+        </Tilt>
     )
 }
 
@@ -48,4 +65,4 @@ const About = () => {
     )
 }
 
-export default About
+export default SectionWrapper(About, "about")
