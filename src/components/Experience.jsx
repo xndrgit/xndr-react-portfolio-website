e8e1bd75ca74b1249b1c221a69db1a9def6b173a
XanderWorld <xander.mymails@gmail.com>
2024-03-26 00:48:31 +0100

    last part of Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import {motion} from "framer-motion";
-
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 import {styles} from "../style.js";
@@ -11,12 +11,44 @@ import {textVariant} from "../utils/motion.js";
 // eslint-disable-next-line react/prop-types
 const ExperienceCard = ({experience}) => (
 
-    <div>
-        <h3 className="text-white text-[24px] font-bold">
+    <VerticalTimelineElement
+        contentStyle={{background: '#1d1836', color: '#fff'}}
+        contentArrowStyle={{borderRight: '7px solid #232631'}}
+        date={experience.date}
+        iconStyle={{background: experience.iconBg}}
+        icon={
+            <div className="flex justify-center items-center w-full h-full">
+                <img
+                    src={experience.icon}
+                    alt={experience.company_name}
+                    className="w-[60%] h-[60%] object-contain"
+                />
+            </div>
+        }
+    >
+
+        <div>
+            <h3 className="text-white text-[24px] font-bold">
+                {/* eslint-disable-next-line react/prop-types */}
+                {experience.title}
+            </h3>
+            <p className="text-secondary text-[16px] font-semibold" style={{margin: 0}}>
+                {/* eslint-disable-next-line react/prop-types */}
+                {experience.company_name}
+            </p>
+        </div>
+
+        <ul className="mt-5 list-disc ml-5 space-y-2">
             {/* eslint-disable-next-line react/prop-types */}
-            {experience.title}
-        </h3>
-    </div>
+            {experience.points.map((point, index) => (
+                <li key={`experience-point-${index}`} className="text-white-100 text-[14px] pl-1 tracking-wider">
+                    {point}
+                </li>
+            ))}
+
+        </ul>
+
+    </VerticalTimelineElement>
 
 )
 
@@ -33,11 +65,11 @@ const Experience = () => {
             </motion.div>
 
             <div className="mt-20 flex flex-col">
-                <div>
+                <VerticalTimeline>
                     {experiences.map((experience, index) => (
                         <ExperienceCard key={index} experience={experience}/>
                     ))}
-                </div>
+                </VerticalTimeline>
             </div>
         </>
     )
