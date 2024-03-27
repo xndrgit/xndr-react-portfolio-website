a39a99df2696deece2423adca4ac82ad6cec65aa
XanderWorld <xander.mymails@gmail.com>
2024-03-27 08:14:14 +0100

    1st part of Work.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {SectionWrapper} from "./hoc/index.js";
+import {motion} from "framer-motion";
+import {textVariant} from "../utils/motion.js";
+import {styles} from "../style.js";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
+    return (
+        <motion.div variants={textVariant()}>
+            <p className={styles.sectionSubText}>
+                My work
+            </p>
+            <h2 className={styles.sectionHeadText}>
+                Projects.
+            </h2>
+        </motion.div>
+
+    )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
